9db89fb2761d08426ae5760d52b8231d0c85bf02
kcs <aerang049@gmail.com>
2023-08-23 15:59:40 +0900

    8월 23일 기준 파일

--- PycharmProjects/pythonProject/230703/3-6 graf.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-import numpy as np
-
-acls = [[x,y]
-        for x,y
-        in zip(np.random.randint(-4, 0, (10)), np.random.randint(5, 10, (10)))
-
-
-]
